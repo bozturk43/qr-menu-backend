ed2a69d379c082f2c41552de2110cf8da6cec660
bozturk43 <burakozturk9294@outlook.com>
2025-06-30 16:10:20 +0300

    0.1.19 - middleware da cloudinary için izinler eklendi.

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,37 @@
+// config/middlewares.ts
+
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  // 'strapi::security' string'ini aşağıdaki obje ile değiştiriyoruz
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true, // Varsayılan politikaları koru
+        directives: {
+          'connect-src': ["'self'", 'https'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            // YENİ EKLENEN KURAL: Cloudinary'nin domain'ini güvenli kaynak olarak ekliyoruz
+            'res.cloudinary.com', 
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            // YENİ EKLENEN KURAL: Medya dosyaları için de ekliyoruz
+            'res.cloudinary.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
